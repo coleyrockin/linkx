654e8a75d617d4a3d4e78f0b3d1f6a6207630629
coleyrockin <coleyrockin@aol.com>
2022-11-16 21:58:42 -0600

    trying to import google font

--- src/components/Everything/Index.js
+++ src/components/Everything/Index.js
@@ -3,32 +3,28 @@ import Boyd from "../../assets/imgs/BoydBGRM.png"
 
 function Everything() {
   return (
-    <div className="App">
-      <body class="background">
-        <div class="centr">
-          <img src={Boyd} alt="Headshot" />
-
+    <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+      <img src={Boyd} alt="Headshot"
+        class="w-20 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+        width="384" height="512" />
+      <section>
+        <div class="container">
+          {/* Google Font? */}
+          <h1 css="font-family: 'Lora', serif">Boyd C Roberts</h1>
+          <h1>Full-stack developer</h1>
+          <button>
+            <a href="https://github.com/coleyrockin">Github</a>
+          </button>
+          <button>
+            <a href="">Portfolio Coming Soon</a>
+          </button>
+          <button>
+            <a href="https://www.linkedin.com/in/boydcroberts/">Linkedin</a>
+          </button>
         </div>
-        <section>
+      </section>
 
-          <div class="container">
-            {/* Google Font? */}
-            <h1>Boyd C Roberts.</h1>
-            <h1>Full-stack developer</h1>
-            <button>
-              <a href="https://github.com/coleyrockin">Github</a>
-            </button>
-            <button>
-              <a href="">Portfolio Coming Soon</a>
-            </button>
-            <button>
-              <a href="https://www.linkedin.com/in/boydcroberts/">Linkedin</a>
-            </button>
-          </div>
-        </section>
-
-      </body>
-    </div>
+    </figure>
   );
 }
 
