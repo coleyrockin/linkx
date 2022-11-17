19a1846a47d4b86d213f831cb55cda7644794a10
coleyrockin <coleyrockin@aol.com>
2022-11-16 22:08:33 -0600

    color schemes

--- src/components/Everything/Index.js
+++ src/components/Everything/Index.js
@@ -3,15 +3,15 @@ import Boyd from "../../assets/imgs/BoydBGRM.png"
 
 function Everything() {
   return (
-    <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+    <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-500">
       <img src={Boyd} alt="Headshot"
         class="w-20 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
         width="384" height="512" />
       <section>
         <div class="container">
           {/* Google Font? */}
-          <h1 css="font-family: 'Lora', serif">Boyd C Roberts</h1>
-          <h1>Full-stack developer</h1>
+          <h1 class="text-center text-4xl text-zinc-100 ">Boyd C Roberts</h1>
+          <h1 class="text-center text-2xl">Full-stack developer</h1>
           <button>
             <a href="https://github.com/coleyrockin">Github</a>
           </button>
