01d136faf407573aae0f6d8dcfafa8c586c3698f
coleyrockin <coleyrockin@aol.com>
2022-11-20 19:24:16 -0600

    colors, transparency

--- src/components/Everything/Index.js
+++ src/components/Everything/Index.js
@@ -12,10 +12,10 @@ const TEXTS = [
   '"Je..."',
   '"I read this book 7 years ago..."',
   '"I dont believe in money, but..."',
-  '"Can I borrow $5000???"',
+  '"Can I borrow..."',
   '"Society is so..."',
   '"One time on a ...',
-  '"If you think enough, nothing makes sense"',
+  '"If you think enough, nothing..."',
   "'Art is by far.."
 ];
 
@@ -43,15 +43,15 @@ function Everything() {
         <h2 class="text-xl text-stone-400">Gang | | Gang </h2>
         <div className='md:flex flex flex-col'>
           <button>
-            <a className='text-stone-500 text-xl relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden font-medium bg-gradient-to-br from-cyan-500 to-blue-500 hover:text-slate-200 focus:outline-none px-5 py-2.5 rounded-md'
+            <a className='text-stone-500 text-xl relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden font-medium bg-gradient-to-br from-cyan-400 to-blue-500 hover:text-slate-800 focus:outline-none px-5 py-2.5 rounded-md'
               href="https://github.com/coleyrockin">Github</a>
           </button>
           <button>
-            <a className='text-stone-700 text-2xl relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden font-medium bg-gradient-to-br from-cyan-500 to-blue-500 hover:text-slate-200 focus:outline-none px-5 py-2.5 rounded-md'
+            <a className='text-stone-700 text-2xl relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden font-medium bg-gradient-to-br from-cyan-500 to-blue-600 hover:text-slate-900 focus:outline-none px-5 py-2.5 rounded-md'
               href="https://coleyrockin.github.io/react-portfolio/">Portfolio</a>
           </button>
           <button>
-            <a className='text-stone-900 text-3xl relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden font-medium bg-gradient-to-br from-cyan-500 to-blue-500 hover:text-slate-200  focus:outline-none px-5 py-2.5 rounded-md'
+            <a className='text-stone-800 text-3xl relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden font-medium bg-gradient-to-br from-cyan-600 to-blue-800 hover:text-slate-300  focus:outline-none px-5 py-2.5 rounded-md'
               href="https://www.linkedin.com/in/boydcroberts/">Linkedin</a>
           </button>
         </div>
