74f24f681731317aad33ab8fa43994babed5438c
coleyrockin <coleyrockin@aol.com>
2022-11-27 18:24:45 -0600

    colors

--- src/components/Everything/Index.js
+++ src/components/Everything/Index.js
@@ -10,7 +10,7 @@ const TEXTS = [
   '"Thats cool, but..."',
   '"That wont exist in 10-50 years..."',
   '"Technology is..."',
-  '"Je..."',
+  '"J..."',
   '"I read this book 7 years ago..."',
   '"I dont believe in..."',
   '"Can I borrow..."',
@@ -29,14 +29,14 @@ function Everything() {
   React.useEffect(() => {
     const intervalId = setInterval(() =>
       setIndex(index => index + 1),
-      300 // every 3 seconds
+      200 // every 3 seconds
     );
     return () => clearTimeout(intervalId);
   }, []);
   return (
     <div class="flex flex-col mx-auto md:container md:mx-auto sm:mx-8 rounded-xl p-1 m-4 bg-gray-800 w-11/12">
-      <img src={Boyd} alt="Headshot" class="w-44 h-auto rounded-full mx-auto m-1 grayscale" />
-      <div class="pt-6 md:p-8 text-center space-y-4 m-1">
+      <img src={Boyd} alt="Headshot" class="w-24 h-auto rounded-full mx-auto m-1 grayscale" />
+      <div class="pt-2 text-center space-y-4">
         <h1 class="text-4xl  text-stone-300 m-1 ">｛Boyd Roberts｝</h1>
         <p className='flex justify-center italic  text-stone-300'>
           <TextTransition springConfig={presets.gentle}>
@@ -44,14 +44,13 @@ function Everything() {
           </TextTransition>
         </p>
         <h2 class="text-xl text-stone-400">Full Stack | | Developer</h2>
-
         <div className='md:flex flex flex-col whitespace-pre'>
           <button>
-            <a className='text-stone-700 text-xl relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden font-medium bg-gradient-to-br from-gray-500 to-slate-500 hover:text-slate-300 focus:outline-none px-5 py-2.5 rounded-md'
+            <a className='text-stone-400 text-xl relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden font-medium bg-gradient-to-br from-gray-500 to-slate-500 hover:text-slate-300 focus:outline-none px-5 py-2.5 rounded-md'
               href="https://github.com/coleyrockin">Github <FaGithub /></a>
           </button>
           <button>
-            <a className='text-stone-500 text-2xl relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden font-medium bg-gradient-to-br from-slate-600 to-gray-600 hover:text-slate-300 focus:outline-none px-5 py-2.5 rounded-md'
+            <a className='text-stone-400 text-2xl relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden font-medium bg-gradient-to-br from-slate-600 to-gray-600 hover:text-slate-300 focus:outline-none px-5 py-2.5 rounded-md'
               href="https://coleyrockin.github.io/react-portfolio/">Portfolio <FaGlobe /></a>
           </button>
           <button>
